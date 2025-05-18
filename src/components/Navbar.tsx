0d4faf7fc08097804a065f04f95793e6f30e35cd
Naominm <naomimbugua536@gmail.com>
2025-05-18 19:46:02 +0300

    Initial Project reset

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import {Box,AppBar} from "@mui/material"
+
+
+
+function NavBar(){
+    return(
+        <Box component="div"sx={{backgroundColor:"Acent", width:"100%"}}>
+        <AppBar sx={{backgroundColor:"teal", padding:2}}>hello </AppBar>
+        
+        </Box>
+    )
+}
+
+export default NavBar;
