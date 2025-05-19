d9b0523fd0863b2f77447d5db0fa03916da62e61
Naominm <naomimbugua536@gmail.com>
2025-05-19 23:18:23 +0300

    Enhance design at the text section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ function AboutSection() {
   return (
     <Box
       component="div"
-      sx={{ display: "flex", gap: "3rem", alignItems: "center" }}
+      sx={{ display: "flex", gap: "3rem", alignItems: "center", mt: 5, mb: 5 }}
     >
       <ImageSection />
       <TextSection />
@@ -13,13 +13,24 @@ function AboutSection() {
 }
 
 function ImageSection() {
-  return <Box component="div"></Box>;
+  return <Box component="div" sx={{ width: "50%" }}></Box>;
 }
 
 function TextSection() {
   return (
-    <Box component="div">
-      <Typography variant="h5" sx={{ color: "var(--primary)" }}>
+    <Box
+      component="div"
+      sx={{
+        width: "50%",
+        display: "flex",
+        flexDirection: "column",
+        gap: "1rem",
+      }}
+    >
+      <Typography
+        variant="h5"
+        sx={{ color: "var(--primary)", fontWeight: 800 }}
+      >
         How ChickHub Supports Farmers
       </Typography>
       <Typography variant="body2">
@@ -27,22 +38,34 @@ function TextSection() {
         We understand the unique challenges you face and have built solutions to
         address them.
       </Typography>
-      <Typography>
-        <CheckCircleIcon />
+      <Typography
+        variant="body2"
+        sx={{ display: "flex", alignItems: "center", gap: "1rem" }}
+      >
+        <CheckCircleIcon sx={{ color: "var(--secondary)" }} />
         Simplified Record Keeping <br />
         No more paper records or complicated spreadsheets.
       </Typography>
-      <Typography>
-        <CheckCircleIcon />
+      <Typography
+        variant="body2"
+        sx={{ display: "flex", alignItems: "center", gap: "1rem" }}
+      >
+        <CheckCircleIcon sx={{ color: "var(--secondary)" }} />
         Data-Driven Decisions <br />
         Access insights that help you optimize your operations.
       </Typography>
-      <Typography>
-        <CheckCircleIcon />
+      <Typography
+        variant="body2"
+        sx={{ display: "flex", alignItems: "center", gap: "1rem" }}
+      >
+        <CheckCircleIcon sx={{ color: "var(--secondary)" }} />
         Community Support <br />
         Connect with other farmers to share knowledge and experiences.
       </Typography>
-      <Button variant="contained" sx={{ backgroundColor: "var(--primary)" }}>
+      <Button
+        variant="contained"
+        sx={{ backgroundColor: "var(--primary)", width: "20rem" }}
+      >
         See How It Works
       </Button>
     </Box>
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,12 +2,14 @@ import { Box } from "@mui/material";
 import NavBar from "../components/Navbar";
 import HeroSection from "../components/hero";
 import FeaturesSection from "../components/Features";
+import AboutSection from "../components/About";
 function LandingPage() {
   return (
     <Box component="div">
       <NavBar />
       <HeroSection />
       <FeaturesSection />
+      <AboutSection />
     </Box>
   );
 }
