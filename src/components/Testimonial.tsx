ba035ad9d7ece7afbeaf48a6d86dc332b6de4e17
Naominm <naomimbugua536@gmail.com>
2025-05-20 08:29:14 +0300

    Add more testimonies at testimonial

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -15,7 +15,13 @@ function TestimonialSection() {
         subHeading="Don't just take our word for it - hear from the farmers and businesses who have"
         subHeading2="worked with us."
       />
-      <Box component="div" display="flex" justifyContent="center" mt="10">
+      <Box
+        component="div"
+        display="flex"
+        justifyContent="center"
+        gap="1rem"
+        mt="10"
+      >
         <TesitimonialCard
           icon={
             <FormatQuoteIcon
@@ -25,6 +31,26 @@ function TestimonialSection() {
           text="ChickHub has revolutionized how I manage my poultry farm. The productivity tracking features alone have helped me increase my egg production by 15%."
           name="Sarah Johnson"
           position="Poultry Farmer, Texas"
+        />
+        <TesitimonialCard
+          icon={
+            <FormatQuoteIcon
+              sx={{ bgcolor: "#fff", color: "var(--primary)" }}
+            />
+          }
+          text="Naomi built our business website and the results have been amazing. Her attention to detail and understanding of our needs exceeded our expectations."
+          name="Michael chen"
+          position="Business Owner, California"
+        />
+        <TesitimonialCard
+          icon={
+            <FormatQuoteIcon
+              sx={{ bgcolor: "#fff", color: "var(--primary)" }}
+            />
+          }
+          text="The market linkage feature has connected me with buyers I wouldn't have found otherwise. My sales have increased and I'm getting better prices for my products"
+          name="Mary Wangui"
+          position="Poultry Farmer, Kenya"
         />
       </Box>
     </Box>
